2a0112fda9f571930abbb417aeba825a8c7f0402
Gustavo Gonzalez <Gegf1234@Gmail.com>
2020-12-14 09:52:40 -0300

    Adding the response to the DOM

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,22 @@
 const xhr = new XMLHttpRequest();
 
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json';
+
+xhr.onload = () => {
+  //   const listOfPosts = JSON.parse(xhr.response);
+  const listOfPosts = xhr.response;
+
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector('h2').textContent = post.title.toUpperCase();
+    postEl.querySelector('p').textContent = post.body;
+    listElement.append(postEl);
+  }
+};
 
 xhr.send();
