a48705b72ed10281c77c4669ce959d08eddc462d
Gustavo Gonzalez <Gegf1234@Gmail.com>
2020-12-14 12:24:19 -0300

    added headers

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,6 +8,8 @@ const postList = document.querySelector('ul');
 const sendHttpRequest = (method, url, data) => {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
     xhr.open(method, url);
 
     xhr.responseType = 'json';
